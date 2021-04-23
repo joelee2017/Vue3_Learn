ee85e3557aed46d70796c61402f86d6a14f953e7
joelee <as955133@gmail.com>
2021-04-23 22:20:01 +0800

    Vue 基礎入門篇 -  拆分組件 - 3(使用SCSS與RWD)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -18,38 +18,111 @@ export default {
         </nav>
       </header>
 </template>
-<style>
+<style lang="scss">
 header {
   background-color: #373c3f;
   width: 100%;
   height: 97px;
-}
-header > nav {
+  @media screen and (max-width: 640px){
+    transition: height 0.2s;
+    height: 37px;
+    overflow: hidden;
+    &.openMenu {
+    height: 232px;
+  }
+  }
+  > nav {
   position: relative;
   width: 1024px;
   height: 100%;
   margin: 0 auto;
-}
-header > nav > h1 {
+  @media screen and (max-width: 1044px){
+    width: 100%;    
+  }
+  @media screen and (max-width: 640px){
+    width: 100%;
+    height: 232px;
+  }
+  > h1 {
   line-height: 97px;
   font-size: 18px;
   float: left;
   color: #fff;
   margin-right: 20px;
+  @media screen and (max-width: 1044px){
+     margin-left: 5%    
+  }
+  @media screen and (max-width: 640px){
+    line-height: 37px;
+  }
 }
-header > nav > div {
+> div {
   width: 100%;
   height: 100%;
-}
-header > nav > div > a {
+  @media screen and (max-width: 640px){
+    clear: both;
+    width: 100%;
+    height: 195px;
+  }
+  > a {
   display: block;
   line-height: 97px;
   font-size: 15px;
   float: left;
   color: #fff;
   padding: 0 10px;
+  @media screen and (max-width: 640px){
+    width: 100%;
+    height: auto;
+    overflow: hidden;
+    line-height: 37px;
+    text-align: center;
+  }
+}
+}
+}
 }
 a#moblie_menu {
   display: none;
+  @media screen and (max-width: 640px){
+    position: absolute;
+    top: 0;
+    right: 0;
+    display: block;
+    width: 37px;
+    height: 37px;
+    background-image: url("~@/assets/images/menu.jpg");
+    background-position: 50% 50%;
+    background-size: 95% auto;
+    background-repeat: no-repeat;
+  }
 }
+// header > nav {
+//   position: relative;
+//   width: 1024px;
+//   height: 100%;
+//   margin: 0 auto;
+// }
+// header > nav > h1 {
+//   line-height: 97px;
+//   font-size: 18px;
+//   float: left;
+//   color: #fff;
+//   margin-right: 20px;
+// }
+// header > nav > div {
+//   width: 100%;
+//   height: 100%;
+// }
+// header > nav > div > a {
+//   display: block;
+//   line-height: 97px;
+//   font-size: 15px;
+//   float: left;
+//   color: #fff;
+//   padding: 0 10px;
+// }
+// a#moblie_menu {
+//   display: none;
+// }
 </style>
