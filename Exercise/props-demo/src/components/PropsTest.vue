6ab1f6fc5cc7969d6cea1d3e0e23b3dfc93af777
joe <as955133@gmail.com>
2021-04-27 21:27:16 +0800

    Vue 基礎入門篇 - 組件資料傳遞Props-3

--- Exercise/props-demo/src/components/PropsTest.vue
+++ Exercise/props-demo/src/components/PropsTest.vue
@@ -2,7 +2,13 @@
 <script>
 export default {
     //與上層相同屬性名稱接值
-    props:['msg'],
+    // props:['msg'], //盡量不要用陣列定義，改使用物件註名型別方式
+    props:{
+        msg:{
+            type:String, //註名型別
+            default:"my name is Joe", //預設值
+        }
+    },
     // 一定要透過setup帶參數回傳上西參數
     setup(props){
         console.log(props)
