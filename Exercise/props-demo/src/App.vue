e951165892c07a1609d48eedb0d085042b6b6c76
joe <as955133@gmail.com>
2021-04-27 21:48:19 +0800

    Vue 基礎入門篇 - 組件資料傳遞Props-5

--- Exercise/props-demo/src/App.vue
+++ Exercise/props-demo/src/App.vue
@@ -1,22 +1,42 @@
 
 <script>
 import PropsTest from"@/components/PropsTest.vue"
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 export default {
   components:{
     PropsTest,
   },
   setup(){
-    const data =ref("hello joe!!");
+    //不同型別傳值
+    const str =ref("hello joe!!");
+    const bool =ref(true)
+    const num =ref(0)
+    const arr = reactive([])
+    const obj = reactive([])
+    const handclickfn = ()=>{
+
+    }
     return{
-      data
+      str,
+      bool,
+      num,
+      arr,
+      obj,
+      handclickfn
     }
   }
 }
 </script>
 <template>
 <!-- 自訂義屬性名稱：msg ，txt沒用bind方式只能寫字串，而且不能用動態方式 -->
-  <PropsTest :msg="data" txt="123456abcd" />
+  <PropsTest  txt="123456abcd"
+    :str="str"
+    :bool="bool"
+    :num="num"
+    :arr="arr"
+    :obj="obj"
+    :handclickfn="handclickfn"
+   />
 </template>
 
 <style lang="scss">
--- Exercise/props-demo/src/components/PropsTest.vue
+++ Exercise/props-demo/src/components/PropsTest.vue
@@ -4,9 +4,29 @@ export default {
     //與上層相同屬性名稱接值
     // props:['msg'], //盡量不要用陣列定義，改使用物件註名型別方式
     props:{
-        msg:{
+        str:{
             type:String, //註名型別
             default:"my name is Joe", //預設值
+        },
+        bool:{
+            type:Boolean, 
+            default:true,
+        },
+        num:{
+            type:Number, 
+            default:0,
+        },
+        arr:{
+            type:Array, 
+            default: ()=>[], // 陣列預設值建議用方法回傳空陣列，不要直接給空陣列
+        },
+        obj:{
+            type:Object, 
+            default:()=>({}),// ({})回傳return 結果
+        },
+        handclickfn:{
+            type:Function, 
+            default:()=>{} // 回傳方法
         },
         txt:String
     },
@@ -21,7 +41,7 @@ export default {
 </script>
 <template>
 <!-- 取出上層傳值 -->
-    <h1>{{props.msg}} {{props.txt}}</h1>
+    <h1>{{props.str}} {{props.txt}}</h1>
 </template>
 <style>
 h1{
