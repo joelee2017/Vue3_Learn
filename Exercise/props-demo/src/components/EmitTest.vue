d1f27f4edfa5d4fba9f4b2392cd0a55273a28606
joe <as955133@gmail.com>
2021-04-29 22:58:13 +0800

    Vue 基礎入門篇 - emits 的使用

--- Exercise/props-demo/src/components/EmitTest.vue
+++ Exercise/props-demo/src/components/EmitTest.vue
@@ -1,9 +1,16 @@
 <script>
 import { onMounted, ref } from 'vue'
 export default {
+    props:{},//父傳子使用
+    emits:{//子傳父使用，可透過這樣的方式往上傳值
+        CallBack:(num)=>{//驗證
+            // console.log("num=> ", num)
+            return num.value === 0;
+        }
+    }, 
     // 透過解構把 {emit}拿出來
     setup(props, {emit}){
-        const num = ref(0);
+        const num = ref(1);
 
         onMounted(()=>{
             //  透過emit傳參數
