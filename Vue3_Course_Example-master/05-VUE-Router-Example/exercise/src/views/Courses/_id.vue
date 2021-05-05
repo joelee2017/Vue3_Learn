e40f0eff8f42d3d41ac5b7f0fa095679f3af10f3
joe <as955133@gmail.com>
2021-05-05 22:29:05 +0800

    Vue 基礎入門篇 - 動態網址案例實作-5(資料處理)

--- Vue3_Course_Example-master/05-VUE-Router-Example/exercise/src/views/Courses/_id.vue
+++ Vue3_Course_Example-master/05-VUE-Router-Example/exercise/src/views/Courses/_id.vue
@@ -1,32 +1,34 @@
 <script>
 import { onMounted, reactive } from 'vue';
-import {useRoute} from "vue-router"
-import axios from "axios"
+import {useRoute} from "vue-router";
+import axios from "axios";
 export default {
   setup() {
-    const route=useRoute();
-    const courses = reactive({data:{}});
+    const route = useRoute();
+    const courses = reactive({ data: {} });
     onMounted(()=>{
       // console.log("route=> ",route.params.id)
-      axios.get(`https://vue-lessons-api.herokuapp.com/courses/${route.params.id}`).then((res)=>{
+      axios
+       .get(`https://vue-lessons-api.herokuapp.com/courses/${route.params.id}`)
+      .then((res)=>{
         //console.log(res);
-        courses.data = res.data[0];
+        courses.data = res.data.data[0];
         console.log('courses.data=> ',courses.data);
       }) 
     })
-    return {};
+    return {courses};
   },
 };
 </script>
 <template>
   <div>
     <div>
-      <h1></h1>
-      <h2>NTD:</h2>
-      <img src="" alt="" />
-      <div>
-        <img src="" alt="" />
-        <p></p>
+      <h1>{{courses.data.name}}</h1>
+      <h2>NTD:{{courses.data.money}}</h2>
+      <img :src="courses.data.photo" alt="" />
+      <div v-if="Object.keys(courses.data).length !== 0">
+        <img :src="courses.data.teacher.img" alt="" />
+        <p>{{courses.data.teacher.name}}</p>
       </div>
     </div>
     <!-- error_message -->
