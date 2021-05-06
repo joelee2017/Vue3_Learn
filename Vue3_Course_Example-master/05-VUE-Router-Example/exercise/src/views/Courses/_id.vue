24cbe607d91f376670a6142b6b5cc9db1963cf7b
joe <as955133@gmail.com>
2021-05-06 21:34:41 +0800

    Vue 基礎入門篇 - 動態網址案例實作-6(API錯誤處理)

--- Vue3_Course_Example-master/05-VUE-Router-Example/exercise/src/views/Courses/_id.vue
+++ Vue3_Course_Example-master/05-VUE-Router-Example/exercise/src/views/Courses/_id.vue
@@ -1,10 +1,11 @@
 <script>
-import { onMounted, reactive } from 'vue';
+import { onMounted, reactive, ref } from 'vue';
 import {useRoute} from "vue-router";
 import axios from "axios";
 export default {
   setup() {
     const route = useRoute();
+    const isError = ref(false);
     const courses = reactive({ data: {} });
     onMounted(()=>{
       // console.log("route=> ",route.params.id)
@@ -14,15 +15,19 @@ export default {
         //console.log(res);
         courses.data = res.data.data[0];
         console.log('courses.data=> ',courses.data);
-      }) 
+      }).catch(error=>{
+          console.log("error=> ",error);
+        isError.value = true;
+        courses.data['error_message'] = error.response.data.error_message;      
+      });
     })
-    return {courses};
+    return {courses,isError};
   },
 };
 </script>
 <template>
   <div>
-    <div>
+    <div v-if="!isError">
       <h1>{{courses.data.name}}</h1>
       <h2>NTD:{{courses.data.money}}</h2>
       <img :src="courses.data.photo" alt="" />
@@ -32,7 +37,7 @@ export default {
       </div>
     </div>
     <!-- error_message -->
-    <h1></h1>
+    <h1 v-if="isError">{{courses.data.error_message}}</h1>
   </div>
 </template>
 
