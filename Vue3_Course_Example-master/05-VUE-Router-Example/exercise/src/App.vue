5302b0dd9daf07f9c5c6c9b058308efcb5fc58f1
joe <as955133@gmail.com>
2021-05-06 22:15:37 +0800

    Vue 基礎入門篇 - 動態網址案例實作-9(router-link選取)

--- Vue3_Course_Example-master/05-VUE-Router-Example/exercise/src/App.vue
+++ Vue3_Course_Example-master/05-VUE-Router-Example/exercise/src/App.vue
@@ -1,18 +1,37 @@
 <script>
+import { ref, watch } from 'vue';
+import {useRoute} from "vue-router"
 export default {
   setup() {
-    return {};
+    // router-link選取，透過 route.path 判段增加 active 樣式
+    const routerArr =['','about','Courses']; // 預存路徑
+    const route = useRoute();
+    const idx =ref(0);
+    watch(()=> route.path, ()=>{
+    // console.log("route.path=> ",route.path);
+      routerArr.forEach((item,index)=>{
+        // 去掉第一個字元
+        const rp = route.path.substr(1).split('/')[0];
+        //console.log('rp=> ', rp);
+        if(rp === item){
+        // console.log(index);
+          idx.value= index;
+        }
+      })
+    })
+
+    return {idx};
   },
 };
 </script>
 
 <template>
   <div id="nav">
-    <router-link to="/"> Home </router-link>
+    <router-link to="/" :class="{active:idx===0}"> Home </router-link>
     |
-    <router-link to="/about"> About </router-link>
+    <router-link to="/about" :class="{active:idx===1}"> About </router-link>
     |
-    <router-link to="/Courses"> Courses </router-link>
+    <router-link to="/Courses" :class="{active:idx===2}"> Courses </router-link>
   </div>
   <router-view />
 </template>
@@ -47,5 +66,8 @@ body {
       color: #42b983;
     }
   }
+  // .router-link-active{
+  //   color: red;
+  // }
 }
 </style>
