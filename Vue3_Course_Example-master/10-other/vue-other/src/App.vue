2c949af32c244a8834eab5876f33b1f70fcd2bfa
joe <as955133@gmail.com>
2021-05-11 22:17:42 +0800

    Vue 基礎入門篇 -  Script Setup-1

--- Vue3_Course_Example-master/10-other/vue-other/src/App.vue
+++ Vue3_Course_Example-master/10-other/vue-other/src/App.vue
@@ -1,16 +1,16 @@
-<script>
+<script setup>
+// script 加上  setup可省略過去的 setup()模式
 import { ref } from "vue";
-export default {
-  setup() {
-    const name = ref("mike");
-    return {
-      name,
-    };
-  },
-};
+// 沒有在模版上使用的變數，就不會在丟到 template 上
+ let timer = null;
+ const name = ref("joe");
+ const handName = ()=>{
+   name.value = 'joe2';
+ }
 </script>
 <template>
   <h1 class="title">{{ name }}</h1>
+  <button @click="handName">add</button>
 </template>
 
 <style lang="scss">
